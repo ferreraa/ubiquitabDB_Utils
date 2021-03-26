3d1466041731ef97c55b4e71526335b9ea750bd7
Alexandre Ferrera <alexandre78ferrera@gmail.com>
2021-03-26 21:55:05 +0100

    User object updated
    hash and salt now available within User class (will be necessary to check password)

--- src/user.ts
+++ src/user.ts
@@ -1,8 +1,10 @@
 export class User {
   private _email: string;
   private _name: string;
+  private _hash: string;
+  private _salt: string;
 
-  constructor(email: string, name: string) {
+  constructor(email: string, name: string, hash: string, salt: string) {
     this._email = email;
     this._name = name;
   }
@@ -13,5 +15,13 @@ export class User {
 
   public get name(): string {
     return this._name;
+  }
+
+  public get hash(): string {
+    return this._hash;
+  }
+
+  public get salt(): string {
+    return this._salt;
   }
 }
